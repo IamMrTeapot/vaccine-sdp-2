1772c5151836ad2aecffe4db72c132be7732f6b7
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-08-31 20:16:12 +0700

    feat: :sparkles: create layout and fix page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import MenuBar from "@/components/MenuBar";
 import "./globals.css";
 import type { Metadata } from "next";
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className={styles.pageBackground}></div>
+        <MenuBar />
+        <div className="mt-[75px]">{children}</div>
+      </body>
     </html>
   );
 }
