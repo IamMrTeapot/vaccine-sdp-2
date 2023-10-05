aca06d5f5f8040ab7de0f398ca8c16fcb4b4079e
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-05 20:36:51 +0700

    feat: :sparkles: add PromoteCard to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Banner from "@/components/Banner";
+import { PromoteCard } from "@/components/PromoteCard";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <div className="flex flex-row justify-center">
+        <PromoteCard />
+      </div>
     </main>
   );
 }
