aeeeae3c26b49aaf267753001454fd61f8c386dd
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-05 20:35:58 +0700

    feat: :sparkles: create VlogPlayer component

---
+++ src/components/VlogPlayer.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+export function VlogPlayer({
+  vdoSrc,
+  isPlaying,
+}: {
+  vdoSrc: string;
+  isPlaying: boolean;
+}) {
+  const vdoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (isPlaying) {
+      vdoRef.current?.play();
+    } else {
+      vdoRef.current?.pause();
+    }
+  }, [isPlaying]);
+
+  return (
+    <video className="w-[40%]" src={vdoSrc} ref={vdoRef} controls loop muted />
+  );
+}
