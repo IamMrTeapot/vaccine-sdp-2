0d7d017b1f4fd00ab392db3605bc152dcba7765d
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-05 17:08:49 +0700

    feat: :sparkles: move CardPanel to hospital page

---
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -0,0 +1,10 @@
+import CardPanel from "@/components/CardPanel";
+
+export default function HospitalPage() {
+  return (
+    <main className="p-5">
+      <h1 className="text-center text-5xl font-thin">Hospital List</h1>
+      <CardPanel />
+    </main>
+  );
+}
