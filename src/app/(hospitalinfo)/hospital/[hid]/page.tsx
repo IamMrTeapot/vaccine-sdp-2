9d0bf30455031b3102e9f644a95130bb06916d7c
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-17 10:01:38 +0700

    feat: :sparkles: modify [hid] page

--- src/app/(hospitalinfo)/hospital/[hid]/page.tsx
+++ src/app/(hospitalinfo)/hospital/[hid]/page.tsx
@@ -1,32 +1,17 @@
 import Image from "next/image";
+import getHospital from "@/libs/getHospital";
 
-export default function HospitalDetailPage({
+export default async function HospitalDetailPage({
   params,
 }: {
   params: { hid: string };
 }) {
-  const mockHospitalRepo = new Map();
-  mockHospitalRepo.set("1", {
-    name: "Chulalongkorn Hospital",
-    imgSrc: "/img/chula.jpg",
-  });
-  mockHospitalRepo.set("2", {
-    name: "Rajavithi Hospital",
-    imgSrc: "/img/rajavithi.jpg",
-  });
-  mockHospitalRepo.set("3", {
-    name: "Thammasat University Hospital",
-    imgSrc: "/img/thammasat.jpg",
-  });
-
+  const hospitalDetail = await getHospital(params.hid);
   return (
     <main className="p-5 flex flex-col items-center justify-center">
-      <h1 className="text-lg text-center font-medium">
-        Hospital ID : {params.hid}
-      </h1>
       <div className="flex flex-col my-5 items-center">
         <Image
-          src={mockHospitalRepo.get(params.hid).imgSrc}
+          src={hospitalDetail.data.picture}
           alt="Hospital Image"
           width={0}
           height={0}
@@ -34,7 +19,18 @@ export default function HospitalDetailPage({
           className="rounded-lg w-[30vw] bg-black"
         />
         <div className="text-lg my-5 font-extrabold">
-          {mockHospitalRepo.get(params.hid).name}
+          {hospitalDetail.data.name}
+        </div>
+        <div className="text-m text-center">
+          {hospitalDetail.data.address}
+          <br />
+          {hospitalDetail.data.district}
+          <br />
+          {hospitalDetail.data.province}
+          <br />
+          {hospitalDetail.data.postalcode}
+          <br />
+          {hospitalDetail.data.tel}
         </div>
       </div>
     </main>
