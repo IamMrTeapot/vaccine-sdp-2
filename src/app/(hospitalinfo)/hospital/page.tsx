e7d9cee40d2148631dbb68b250f1812efeb2dcab
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-17 10:29:38 +0700

    [SDP-9]  Assignment 9  (#7)
    * feat: :sparkles: set up next config
    
    * feat: :sparkles: create libs
    
    * feat: :sparkles: modify hospital card
    
    * feat: :sparkles: implement hospital catalog
    
    * feat: :sparkles: modify hospital page
    
    * feat: :sparkles: modify [hid] page
    
    * feat: :sparkles: update workspace
    
    * feat: :sparkles: remove unused params

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -1,10 +1,23 @@
-import CardPanel from "@/components/CardPanel";
+import HospitalCatalog from "@/components/hospitalCatalog";
+import getHospitals from "@/libs/getHospitals";
+import { LinearProgress } from "@mui/material";
+import { Suspense } from "react";
 
 export default function HospitalPage() {
+  const hospitals = getHospitals();
+
   return (
     <main className="p-5">
       <h1 className="text-center text-5xl font-thin">Hospital List</h1>
-      <CardPanel />
+      <Suspense
+        fallback={
+          <p>
+            Loading ... <LinearProgress />
+          </p>
+        }
+      >
+        <HospitalCatalog hospitalJson={hospitals} />
+      </Suspense>
     </main>
   );
 }
