fed4c3071c493930f536bff8be766fcb545d246b
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-19 20:49:55 +0700

    feat: :sparkles: update menu bar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import MenuItem from "./MenuItem";
+import { Link } from "@mui/material";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default function MenuBar() {
+export default async function MenuBar() {
+
+  const session = await getServerSession(authOptions)
+
   return (
     <div className="h-[75px] w-full bg-blue-300 flex flex-row justify-end gap-6 items-center fixed top-0 left-0 z-10">
       <MenuItem />
@@ -13,6 +19,14 @@ export default function MenuBar() {
         height={0}
         sizes="100%"
       />
+      { session ? <div className="px-8 text-white font-bold tracking-widest 
+      text-2xl absolute z-[200] left-0 top-[20px]"
+      ><Link href='/api/auth/signout'>Sign Out</Link>
+      </div> : <div className="px-8 text-white font-bold tracking-widest 
+      text-2xl absolute z-[200] left-0 top-[20px]"
+      ><Link href='/api/auth/signin'>Sign In</Link>
+      </div>}
+
     </div>
   );
 }
