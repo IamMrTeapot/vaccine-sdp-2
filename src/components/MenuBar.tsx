63dab81cd48366561049d16fc7a2e78da1a10fe2
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-10-29 02:49:27 +0700

    feat: :sparkles: update menu bar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import MenuItem from "./MenuItem";
-import { Link } from "@mui/material";
+import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function MenuBar() {
-
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="h-[75px] w-full bg-blue-300 flex flex-row justify-end gap-6 items-center fixed top-0 left-0 z-10">
@@ -19,14 +18,19 @@ export default async function MenuBar() {
         height={0}
         sizes="100%"
       />
-      { session ? <div className="px-8 text-white font-bold tracking-widest 
-      text-2xl absolute z-[200] left-0 top-[20px]"
-      ><Link href='/api/auth/signout'>Sign Out</Link>
-      </div> : <div className="px-8 text-white font-bold tracking-widest 
-      text-2xl absolute z-[200] left-0 top-[20px]"
-      ><Link href='/api/auth/signin'>Sign In</Link>
-      </div>}
-
+      <div
+        className="px-8 text-white font-bold tracking-widest 
+      text-2xl absolute z-[200] left-0 top-[20px] flex flex-row"
+      >
+        {session ? (
+          <Link href="/api/auth/signout">Sign Out</Link>
+        ) : (
+          <Link href="/api/auth/signin">Sign In</Link>
+        )}
+        <div className="px-8">
+          <Link href="mybooking">My Booking</Link>
+        </div>
+      </div>
     </div>
   );
 }
